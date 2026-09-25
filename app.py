670b258e25387c1cd249ec412c8bf8264c54ea62
Amit pant <139322726+pantamit2003@users.noreply.github.com>
2026-09-25 12:53:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ import threading
 import pytz
 import base64
 import os
+from supabase import create_client
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
 
 # ============================================================
 # CONFIG
